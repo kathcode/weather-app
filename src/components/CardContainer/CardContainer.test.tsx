a066a84f176d1686d22e3ea3325e6ca4c6a062ce
unknown <kathmene27@gmail.com>
2022-07-31 20:28:37 -0400

    Add Card component

--- src/components/CardContainer/CardContainer.test.tsx
+++ src/components/CardContainer/CardContainer.test.tsx
@@ -5,9 +5,19 @@ import CardContainer from './CardContainer';
 
 test('render the color sended by props', () => {
   const bgColor = '#eeeeee';
-  render(<CardContainer bgColor={bgColor}>Title</CardContainer>);
+  render(<CardContainer bgColor={bgColor}><h1>Title</h1></CardContainer>);
 
   const card = screen.getByTestId('card-container');
 
   expect(card).toHaveStyle(`background-color: ${bgColor}`);
 });
+
+test('should render the default color if no value is sended in bgColor prop', () => {
+  render(<CardContainer><h1>Title</h1></CardContainer>);
+
+  const card = screen.getByTestId('card-container');
+
+  expect(card).toHaveStyle(`background-color: #cccccc`);
+});
+
+
--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import ICardContainer from '../../types/Card';
+
+
+// Styled components
 export const CardContainerStyled = styled.section`
-  background-color: ${(props: any) => props.bgColor};
+  background-color: ${(props: any) => props.bgColor || '#cccccc'};
 `;
 
-export default function CardContainer({ bgColor, children }) {
+export default function CardContainer({ bgColor, children }: ICardContainer) {
   return (
-    <CardContainerStyled data-testid="card-container">
+    <CardContainerStyled bgColor={bgColor} data-testid="card-container">
       {children}
     </CardContainerStyled>
   );
